7a60f9046eda6a2e720e10d7f5740e4091e3d6f4
Yiting Xiao <yitingxiao0531@gmail.com>
2024-06-26 13:25:05 -0400

    refactor: edit works page

--- server.py
+++ server.py
@@ -24,6 +24,7 @@ def write_to_file(data):
         message = data['message']
         file = database.write(f'\n{email}, {subject}, {message}')
 
+
 def write_to_csv(data):
     with open('database.csv', mode='a', newline='') as database2:
         email = data['email']
@@ -31,8 +32,6 @@ def write_to_csv(data):
         message = data['message']
         csv_writer = csv.writer(database2, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
         csv_writer.writerow([email, subject, message])
-
-
 
 
 @app.route('/submit_form', methods=['POST', 'GET'])
